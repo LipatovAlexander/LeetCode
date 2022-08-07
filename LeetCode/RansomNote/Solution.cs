73c0d233c3b4baa3c88bf15e44dcf2bc0bb036de
Lipatov Alexander <lipatov.work@bk.ru>
2022-08-08 02:49:31 +0300

    done: Ransom Note

---
+++ LeetCode/RansomNote/Solution.cs
@@ -0,0 +1,36 @@
+﻿namespace LeetCode.RansomNote;
+
+public class Solution
+{
+    public bool CanConstruct(string a, string b)
+    {
+        var dictA = CountNumberOfLetters(a);
+        var dictB = CountNumberOfLetters(b);
+
+        foreach (var c in dictA.Keys)
+        {
+            var inA = dictA[c];
+            var inB = dictB.GetValueOrDefault(c, 0);
+
+            if (inA > inB)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    private static Dictionary<char, int> CountNumberOfLetters(string str)
+    {
+        var dict = new Dictionary<char, int>();
+
+        foreach (var c in str)
+        {
+            var prev = dict.GetValueOrDefault(c, 0);
+            dict[c] = prev + 1;
+        }
+
+        return dict;
+    }
+}

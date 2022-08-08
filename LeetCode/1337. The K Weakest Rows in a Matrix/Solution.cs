62a6c3a060bff4f84fcf1a01ff8ce563099b0cb6
Lipatov Alexander <lipatov.work@bk.ru>
2022-08-08 03:39:34 +0300

    done: 1337

---
+++ LeetCode/1337. The K Weakest Rows in a Matrix/Solution.cs
@@ -0,0 +1,16 @@
+﻿namespace LeetCode._1337._The_K_Weakest_Rows_in_a_Matrix;
+
+// https://leetcode.com/problems/the-k-weakest-rows-in-a-matrix/
+public class Solution
+{
+    public int[] KWeakestRows(int[][] matrix, int k)
+    {
+        return matrix
+            .Select((row, i) => new {Count = row.Count(c => c == 1), Index = i})
+            .OrderBy(row => row.Count)
+            .ThenBy(row => row.Index)
+            .Select(row => row.Index)
+            .Take(k)
+            .ToArray();
+    }
+}

086a9faf30e87ef538ea234687ffd7d0a1d0fedc
Lipatov Alexander <lipatov.work@bk.ru>
2022-08-08 03:49:22 +0300

    done: 1672

---
+++ LeetCode/1672. Richest Customer Wealth/Solution.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode._1672._Richest_Customer_Wealth;
+
+// https://leetcode.com/problems/richest-customer-wealth/
+public class Solution
+{
+    public int MaximumWealth(int[][] accounts)
+    {
+        return accounts
+            .Select(account => account.Sum())
+            .Max();
+    }
+}

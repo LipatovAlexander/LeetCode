1abd15b22c12141edcd69ce629213da0650fb335
Lipatov Alexander <lipatov.work@bk.ru>
2022-08-08 03:10:37 +0300

    done: Fizz Buzz

---
+++ LeetCode/FizzBuzz/Solution.cs
@@ -0,0 +1,37 @@
+﻿using System.Text;
+
+namespace LeetCode.FizzBuzz;
+
+public class Solution
+{
+    public IList<string> FizzBuzz(int n)
+    {
+        const string fizz = "Fizz";
+        const string buzz = "Buzz";
+
+        var answer = new string[n];
+
+        for (var i = 1; i <= n; i++)
+        {
+            var sb = new StringBuilder();
+            if (i % 3 == 0)
+            {
+                sb.Append(fizz);
+            }
+
+            if (i % 5 == 0)
+            {
+                sb.Append(buzz);
+            }
+
+            if (i % 3 != 0 && i % 5 != 0)
+            {
+                sb.Append(i);
+            }
+
+            answer[i - 1] = sb.ToString();
+        }
+
+        return answer;
+    }
+}

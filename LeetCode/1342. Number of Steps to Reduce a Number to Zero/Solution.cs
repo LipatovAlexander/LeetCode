86cd536cbb91f78cd59ab2e47ba7f1cb3b967959
Lipatov Alexander <lipatov.work@bk.ru>
2022-08-08 03:46:00 +0300

    done: 1342

---
+++ LeetCode/1342. Number of Steps to Reduce a Number to Zero/Solution.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetCode._1342._Number_of_Steps_to_Reduce_a_Number_to_Zero;
+
+// https://leetcode.com/problems/number-of-steps-to-reduce-a-number-to-zero/
+public class Solution
+{
+    public int NumberOfSteps(int num)
+    {
+        var steps = 0;
+        while (num is not 0)
+        {
+            num = num % 2 == 0 ? num / 2 : num - 1;
+            steps++;
+        }
+
+        return steps;
+    }
+}

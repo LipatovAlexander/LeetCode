0d37e78af6b6cd04cd0506fbfc936b05cff1f501
Lipatov Alexander <lipatov.work@bk.ru>
2022-08-09 21:45:50 +0300

    done: 278

---
+++ LeetCode/278. First Bad Version/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode._278._First_Bad_Version;
+
+public class Solution : VersionControl
+{
+    public int FirstBadVersion(int n)
+    {
+        return FirstBadVersion(1, n);
+    }
+
+    private int FirstBadVersion(int left, int right)
+    {
+        if (left >= right)
+        {
+            return left;
+        }
+
+        var middle = left + (right - left) / 2;
+        return IsBadVersion(middle)
+            ? FirstBadVersion(left, middle)
+            : FirstBadVersion(middle + 1, right);
+    }
+}
---
+++ LeetCode/278. First Bad Version/VersionControl.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetCode._278._First_Bad_Version;
+
+public abstract class VersionControl
+{
+    protected bool IsBadVersion(int version)
+    {
+        throw new NotImplementedException();
+    }
+}

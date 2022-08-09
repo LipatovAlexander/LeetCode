05f670056aa49a1925da188209824399870c0c12
Lipatov Alexander <lipatov.work@bk.ru>
2022-08-09 18:05:30 +0300

    done: 704

---
+++ LeetCode/704. Binary Search/Solution.cs
@@ -0,0 +1,27 @@
+﻿namespace LeetCode._704._Binary_Search;
+
+public class Solution
+{
+    public int Search(int[] nums, int target)
+    {
+        return Search(nums, target, 0, nums.Length);
+    }
+
+    private int Search(int[] nums, int target, int left, int right)
+    {
+        if (left == right)
+        {
+            return -1;
+        }
+
+        if (left == right - 1)
+        {
+            return target == nums[left] ? left : -1;
+        }
+
+        var middle = (right + left) / 2;
+        return target < nums[middle]
+            ? Search(nums, target, left, middle)
+            : Search(nums, target, middle, right);
+    }
+}

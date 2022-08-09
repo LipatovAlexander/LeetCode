e031c8a0b9945774f51431b66e08e763e588975b
Lipatov Alexander <lipatov.work@bk.ru>
2022-08-09 21:58:03 +0300

    done: 35

---
+++ LeetCode/35. Search Insert Position/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode._35._Search_Insert_Position;
+
+public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        return SearchInsert(nums, target, 0, nums.Length);
+    }
+
+    private static int SearchInsert(int[] nums, int target, int left, int right)
+    {
+        if (left >= right - 1)
+        {
+            return target <= nums[left] ? left : right;
+        }
+
+        var middle = left + (right - left) / 2;
+        return target < nums[middle]
+            ? SearchInsert(nums, target, left, middle)
+            : SearchInsert(nums, target, middle, right);
+    }
+}

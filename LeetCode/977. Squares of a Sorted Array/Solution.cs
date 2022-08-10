b400e8280406f87fad1e910568a67c03a911ee6a
Lipatov Alexander <lipatov.work@bk.ru>
2022-08-10 18:59:57 +0300

    done: 977

---
+++ LeetCode/977. Squares of a Sorted Array/Solution.cs
@@ -0,0 +1,58 @@
+﻿namespace LeetCode._977._Squares_of_a_Sorted_Array;
+
+public class Solution
+{
+    public int[] SortedSquares(int[] nums)
+    {
+        var absMinIndex = FindAbsMinIndex(nums);
+        var result = new List<int>
+        {
+            nums[absMinIndex] * nums[absMinIndex]
+        };
+
+        var left = absMinIndex - 1;
+        var right = absMinIndex + 1;
+
+        while (left >= 0 && right < nums.Length)
+        {
+            var leftNum = nums[left];
+            var rightNum = nums[right];
+
+            if (Math.Abs(leftNum) < Math.Abs(rightNum))
+            {
+                result.Add(leftNum * leftNum);
+                left--;
+            }
+            else
+            {
+                result.Add(rightNum * rightNum);
+                right++;
+            }
+        }
+
+        while (left >= 0)
+        {
+            result.Add(nums[left] * nums[left]);
+            left--;
+        }
+
+        while (right < nums.Length)
+        {
+            result.Add(nums[right] * nums[right]);
+            right++;
+        }
+
+        return result.ToArray();
+    }
+
+    private int FindAbsMinIndex(int[] nums)
+    {
+        var i = 0;
+        while (i < nums.Length - 1 && Math.Abs(nums[i]) >= Math.Abs(nums[i + 1]))
+        {
+            i++;
+        }
+
+        return i;
+    }
+}

e23e397591117f02a0f39e8df2559130afdb37ea
Lipatov Alexander <lipatov.work@bk.ru>
2022-08-11 22:42:18 +0300

    done: 283 Move zeroes

---
+++ LeetCode/283. Move Zeroes/Solution.cs
@@ -0,0 +1,14 @@
+﻿namespace LeetCode._283._Move_Zeroes;
+
+public class Solution
+{
+    public void MoveZeroes(int[] nums)
+    {
+        for (int i = 0, cur = 0; i < nums.Length; i++)
+        {
+            if (nums[i] == 0) continue;
+            (nums[cur], nums[i]) = (nums[i], nums[cur]);
+            cur++;
+        }
+    }
+}

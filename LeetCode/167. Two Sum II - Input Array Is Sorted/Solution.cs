622c6d084a3e0d9ddc0943d6c1ac88236d15919d
Lipatov Alexander <lipatov.work@bk.ru>
2022-08-11 22:51:59 +0300

    done: 167 Two Sum II

---
+++ LeetCode/167. Two Sum II - Input Array Is Sorted/Solution.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetCode._167._Two_Sum_II___Input_Array_Is_Sorted;
+
+public class Solution
+{
+    public int[] TwoSum(int[] numbers, int target)
+    {
+        var left = 0;
+        var right = numbers.Length - 1;
+
+        var sum = numbers[left] + numbers[right];
+        while (sum != target)
+        {
+            if (sum > target)
+                right--;
+            else
+                left++;
+            sum = numbers[left] + numbers[right];
+        }
+
+        return new[] {left + 1, right + 1};
+    }
+}

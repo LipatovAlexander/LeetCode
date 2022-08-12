c4871bfa7986ec1549b3c192dc9ff995396ad19b
Lipatov Alexander <lipatov.work@bk.ru>
2022-08-13 00:00:56 +0300

    done: 344 Reverse string

---
+++ LeetCode/344. Reverse String/Solution.cs
@@ -0,0 +1,12 @@
+﻿namespace LeetCode._344._Reverse_String;
+
+public class Solution
+{
+    public void ReverseString(char[] s)
+    {
+        for (var i = 0; i < s.Length / 2; i++)
+        {
+            (s[i], s[^(i + 1)]) = (s[^(i + 1)], s[i]);
+        }
+    }
+}

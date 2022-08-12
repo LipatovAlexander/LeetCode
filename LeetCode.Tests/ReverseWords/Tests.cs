779caba9c48a656266f07b5a75bc3cdd45594da9
Lipatov Alexander <lipatov.work@bk.ru>
2022-08-13 00:42:08 +0300

    done: 557 Reverse words

---
+++ LeetCode.Tests/ReverseWords/Tests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using LeetCode._557._Reverse_Words_in_a_String_III;
+
+namespace LeetCode.Tests.ReverseWords;
+
+public class Tests
+{
+    [Fact]
+    public void Test()
+    {
+        var solution = new Solution();
+        const string str = "fuck you";
+        var result = solution.ReverseWords(str);
+
+        result.Should().Be("kcuf uoy");
+    }
+}
---
+++ LeetCode/557. Reverse Words in a String III/Solution.cs
@@ -0,0 +1,40 @@
+﻿namespace LeetCode._557._Reverse_Words_in_a_String_III;
+
+public class Solution
+{
+    public string ReverseWords(string s)
+    {
+        return new string(ReverseWords(s.ToCharArray()));
+    }
+
+    private static char[] ReverseWords(char[] s)
+    {
+        var start = 0;
+
+        for (var end = 0; end < s.Length; end++)
+        {
+            if (char.IsWhiteSpace(s[end]))
+            {
+                Reverse(s, start, end - 1);
+                end++;
+                start = end;
+            }
+            else if (end == s.Length - 1)
+            {
+                Reverse(s, start, end);
+            }
+        }
+
+        return s;
+    }
+
+    private static void Reverse(IList<char> s, int start, int end)
+    {
+        var length = end - start + 1;
+        var steps = length / 2;
+        for (var i = 0; i < steps; i++)
+        {
+            (s[start + i], s[end - i]) = (s[end - i], s[start + i]);
+        }
+    }
+}

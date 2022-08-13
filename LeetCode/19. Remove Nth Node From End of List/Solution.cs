a73153264c92406efa4d3912b99ebc74de372e4b
Lipatov Alexander <lipatov.work@bk.ru>
2022-08-14 00:39:33 +0300

    done: 19 Remove N-th node from end of list

---
+++ LeetCode/19. Remove Nth Node From End of List/Solution.cs
@@ -0,0 +1,30 @@
+﻿namespace LeetCode._19._Remove_Nth_Node_From_End_of_List;
+
+public class Solution
+{
+    public ListNode? RemoveNthFromEnd(ListNode head, int n)
+    {
+        var first = head;
+        var second = head;
+
+        for (var i = 0; i < n; i++)
+        {
+            second = second?.next;
+        }
+
+        while (second?.next is not null)
+        {
+            first = first.next!;
+            second = second.next;
+        }
+        
+        if (second is null)
+        {
+            return head.next;
+        }
+        
+        first.next = first.next?.next;
+        
+        return head;
+    }
+}

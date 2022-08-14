970989a4931a656be58636a709d64a1d0fce73b3
Lipatov Alexander <lipatov.work@bk.ru>
2022-08-15 00:14:19 +0300

    done: 3. Longest substring without repeating characters

---
+++ LeetCode.Tests/LongestSubstringWithoutRepeatingCharacters/Tests.cs
@@ -0,0 +1,17 @@
+﻿using FluentAssertions;
+using LeetCode._3._Longest_Substring_Without_Repeating_Characters;
+
+namespace LeetCode.Tests.LongestSubstringWithoutRepeatingCharacters;
+
+public class Tests
+{
+    [Theory]
+    [InlineData("uqinntq", 4)]
+    public void SimpleTest(string input, int expected)
+    {
+        var solution = new Solution();
+        var result = solution.LengthOfLongestSubstring(input);
+
+        result.Should().Be(expected);
+    }
+}
---
+++ LeetCode/3. Longest Substring Without Repeating Characters/Solution.cs
@@ -0,0 +1,24 @@
+﻿namespace LeetCode._3._Longest_Substring_Without_Repeating_Characters;
+
+public class Solution
+{
+    public int LengthOfLongestSubstring(string s)
+    {
+        var maxLength = 0;
+        var startPosition = 0;
+        var hashSet = new HashSet<char>();
+
+        foreach (var next in s)
+        {
+            while (!hashSet.Add(next))
+            {
+                hashSet.Remove(s[startPosition]);
+                startPosition++;
+            }
+
+            maxLength = Math.Max(maxLength, hashSet.Count);
+        }
+
+        return maxLength;
+    }
+}
